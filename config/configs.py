ecde12ee254262321aecc09d2c702118b41e5dbe
unknown <tun.kut@outlook.com>
2023-10-12 02:12:19 +0700

    clean main

--- config/configs.py
+++
@@ -1,4 +0,0 @@
-IMAGE_SIZE = 384
-collection_name = 'Glimpse'
-model_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_base_retrieval_coco.pth'
-dataset_path = "./" #Import your dataset here
